deddb6c0d3d5d1c1704cf560c13cf8ab6ce6cbe2
AnasElsayedAli <anas.say3d@gmail.com>
2026-08-02 16:16:52 +0300

    bundles ENHANCED3

--- components/OffersShowcase.tsx
+++ components/OffersShowcase.tsx
@@ -80,7 +80,7 @@ export default function OffersShowcase({ offers, onViewOffer, onAddToCart }: Off
 
         <div className="absolute inset-0 flex flex-col justify-end p-3 sm:p-7">
           {/* Plain (non-heading) tag: global CSS forces h1-h6 to dark charcoal via !important, which would hide white text over this dark banner overlay */}
-          <p className="text-sm sm:text-2xl font-black text-[rgb(255,255,255)] mb-1.5 sm:mb-2 leading-snug drop-shadow-[0_2px_10px_rgba(0,0,0,0.9)] line-clamp-2">
+          <p className="text-xl sm:text-2xl font-black text-[rgb(255,255,255)] mb-1.5 sm:mb-2 leading-snug drop-shadow-[0_2px_10px_rgba(0,0,0,0.9)] line-clamp-2">
             {offer.name}
           </p>
 
